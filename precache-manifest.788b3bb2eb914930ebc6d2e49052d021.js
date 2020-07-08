0f9c24187465b3178ce98fd17d6afbec4ee91d1e
pashadidychenko <pasha.didychenko@gmail.com>
2020-07-08 09:49:37 +0300

    Updates

---
+++ precache-manifest.788b3bb2eb914930ebc6d2e49052d021.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25b6777c19a55b9cf33505c40855eae0",
+    "url": "/pashadidychenko/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "5e26ab80fd0b585378cb",
+    "url": "/pashadidychenko/goit-react-hw-03-image-finder/static/js/2.00a8171f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pashadidychenko/goit-react-hw-03-image-finder/static/js/2.00a8171f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3b1605a50da1d93bf3c",
+    "url": "/pashadidychenko/goit-react-hw-03-image-finder/static/js/main.a5cb4646.chunk.js"
+  },
+  {
+    "revision": "c1a1080418300fd169f2",
+    "url": "/pashadidychenko/goit-react-hw-03-image-finder/static/js/runtime-main.eb65ba4d.js"
+  }
+]);
